be5ac75fdc3dc7f9e6bfadfc8b46ab717031a8fc
Hakry <Hakry@swgemu.com>
2020-10-05 11:56:58 -0400

    [changed] Named Crystals now have their own loot group

--- MMOCoreORB/bin/scripts/loot/groups/color_crystals.lua
+++ MMOCoreORB/bin/scripts/loot/groups/color_crystals.lua
@@ -5,24 +5,7 @@ color_crystals = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "force_color_crystal", weight = 7000000},
-		{itemTemplate = "force_crystal_baass_wisdom", weight = 176471},
-		{itemTemplate = "force_crystal_banes_heart", weight = 176471},
-		{itemTemplate = "force_crysta;_bnars_sacrifice", weight = 176471},
-		{itemTemplate = "force_crystal_dawn_of_dagobah", weight = 176471},
-		{itemTemplate = "force_crystal_gallias_intuition", weight = 176471},
-		{itemTemplate = "force_crystal_horns_future", weight = 176471},
-		{itemTemplate = "force_crystal_kenobis_legacy", weight = 176471},
-		{itemTemplate = "force_crystal_kits_ferocity", weight = 176471},
-		{itemTemplate = "force_crystal_kuns_blood", weight = 176471},
-		{itemTemplate = "force_crystal_mauls_vengence", weight = 176471},
-		{itemTemplate = "force_crystal_mundis_response", weight = 176470},
-		{itemTemplate = "force_crystal_prowess_of_plo_koon", weight = 176470},
-		{itemTemplate = "force_crystal_qui_gons_devotion", weight = 176470},
-		{itemTemplate = "force_crystal_quintessebce_of_the_force", weight = 176470},
-		{itemTemplate = "force_crystal_strength_of_luminaria", weight = 176470},
-		{itemTemplate = "force_crystal_ulics_redemption", weight = 176470},
-		{itemTemplate = "force_crystal_windus_guile", weight = 176470},
+		{itemTemplate = "force_color_crystal", weight = 10000000},
 	}
 }
 
